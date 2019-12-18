e2115bb6f64e2f8a75776b2a42119ff99980479c
Rokas <38216453+rokbag1@users.noreply.github.com>
2019-12-18 18:24:12 +0200

    Last commit to master (I hope so)

---
+++ src/main/main-design.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './main-design.scss'
+
+export default function MainDesign() {
+    return (
+        //Main stuff in website
+        <div className="body-class">
+            <div className="body-title">
+                <h4> Heroes of Might and Magic III calculator </h4>
+            </div>
+        </div>
+    )
+}
