1af7b34362d4e5d32a42b501a11d239d27e46a1e
Rokas <38216453+rokbag1@users.noreply.github.com>
2019-12-24 13:46:06 +0200

    Adding castle hook

--- src/components/creature/creature-design.tsx
+++ src/components/creature/creature-design.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
-export interface Castle  {
+export interface Castle {
     id: number;
     name: string;
     keyWord: string;
     town: string;
-};
+}
 
 const castles: Castle[] = [
     {
@@ -27,7 +27,7 @@ const castles: Castle[] = [
         name: "Cove",
         keyWord: "CA",
         town: "cove"
-    }, 
+    },
 
     {
         id: 4,
@@ -49,7 +49,7 @@ const castles: Castle[] = [
         keyWord: "CA",
         town: "necropolis"
     },
-    // CAnt find neutral 
+    // CAnt find neutral
     // {
     //     id: 7,
     //     name: "Neutral",
@@ -83,26 +83,34 @@ const castles: Castle[] = [
         name: "Dungeon",
         keyWord: "CA",
         town: "dungeon"
-    },
-    
+    }
 ];
 
-//THATS how react is working with object arrays
-const SelectCastle = (): JSX.Element => (
+const SelectCastle = (props: SelectCastleProps): JSX.Element => (
     <div className="castle-images">
         {castles.map(item => (
-            <div className="castle-image" key={item.id}><img src={`src/images/towns/${item.town}.gif`} alt="image" /></div>
+            <div className={`castle-image ${item.id === props.castle?.id? "selected-castle" :""}`} key={item.id} onClick={() => props.setCastle(item)}>
+                <img src={`src/images/towns/${item.town}.gif`} alt="image" />
+            </div>
         ))}
     </div>
 );
 
+interface SelectCastleProps {
+    setCastle: React.Dispatch<React.SetStateAction<Castle | null>>;
+    castle: Castle | null;
+}
+
 export const CreatureDesign: React.FC = () => {
-    //TODO Reikia visų pilių imigų
+    // useState hook to save castle. castle => Selected castle, setCastle => function which do stuff for castle
+    const [castle, setCastle] = useState<Castle | null>(null);
+
     // React way foreach elements
     return (
         <div>
             <div className="select-castle">First select town</div>
-            <SelectCastle />
+            <SelectCastle setCastle={setCastle}  castle={castle}/>
+            {castle != null ? <div>{castle.name}</div> : null}
         </div>
     );
 };
