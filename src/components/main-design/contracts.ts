ac858133e44004b673854fb5b35ddf1eae5af0ca
Rokas <38216453+rokbag1@users.noreply.github.com>
2019-12-27 21:47:36 +0200

    Renaming and adding new key

--- src/components/main-design/contracts.ts
+++ src/components/main-design/contracts.ts
@@ -9,4 +9,5 @@ export interface Creature {
     id: number;
     name: string;
     power: number;
+    image: string;
 }
--- src/shared/creatures/castle.ts
+++ src/shared/creatures/castle.ts
@@ -5,83 +5,97 @@ export const CASTLE_CREATURES: Creature[] = [
         id: 101,
         name: "Pikeman",
         power: 80,
+        image: "CA1"
     },
 
     {
         id: 102,
         name: "Halberdier",
         power: 115,
+        image: "CA2"
     },
 
     {
         id: 103,
         name: "Archer",
         power: 126,
+        image: "CA3"
     },
 
     {
         id: 104,
         name: "Marksman",
         power: 184,
+        image: "CA4"
     },
 
     {
         id: 105,
         name: "Griffin",
         power: 351,
+        image: "CA5"
     },
 
     {
         id: 106,
         name: "Royal Griffin",
         power: 448,
+        image: "CA6"
     },
 
     {
         id: 107,
         name: "Swordsman",
         power: 445,
+        image: "CA7"
     },
 
   {
         id: 108,
         name: "Crusader",
         power: 588,
+        image: "CA8"
     },
 
     {
         id: 109,
         name: "Monk",
         power: 582,
+        image: "CA9"
     },
 
     {
         id: 110,
         name: "Zealot",
         power: 750,
+        image: "CA10"
     },
 
     {
         id: 111,
         name: "Cavalier",
         power: 1946,
+        image: "CA11"
     },
 
     {
         id: 112,
         name: "Champion",
         power: 2100,
+        image: "CA12"
     },
 
     {
         id: 113,
         name: "Angel",
         power: 5019,
+        image: "CA13"
     },
 
     {
         id: 114,
         name: "Archangel",
         power: 8776,
+        image: "CA14"
     },
 ];
