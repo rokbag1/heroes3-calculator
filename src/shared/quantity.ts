bb3c14fce058adac212f7d9e0c4e37f5a90f2b54
Denis Jankelaic <d3nziss@gmail.com>
2020-01-03 22:20:41 +0200

    Added quantity images.

--- src/shared/quantity.ts
+++ src/shared/quantity.ts
@@ -3,69 +3,62 @@ import { Quantity } from "src/components/main-design/contracts";
 export const QUANTITY: Quantity[] = [
     {
         id: 50,
-        name: "Few",
+        name: "few",
         quantityMin: 1,
         quantityMax: 4
     },
 
     {
         id: 51,
-        name: "Several",
+        name: "several",
         quantityMin: 5,
         quantityMax: 9
     },
 
     {
         id: 53,
-        name: "Pack",
+        name: "pack",
         quantityMin: 10,
         quantityMax: 19
     },
 
     {
         id: 54,
-        name: "Lots",
+        name: "lots",
         quantityMin: 20,
         quantityMax: 49
     },
 
     {
         id: 55,
-        name: "Horde",
+        name: "horde",
         quantityMin: 50,
         quantityMax: 99
     },
 
     {
         id: 56,
-        name: "Throng",
+        name: "throng",
         quantityMin: 100,
         quantityMax: 249
     },
-    // CAnt find neutral
-    // {
-    //     id: 7,
-    //     keyWord: "CA",
-    //     town: "neutral"
-    // },
-
     {
-        id: 58,
-        name: "Swarm",
-        quantityMin: 256,
+        id: 57,
+        name: "swarm",
+        quantityMin: 250,
         quantityMax: 499
     },
 
     {
-        id: 59,
-        name: "Zounds",
+        id: 58,
+        name: "zounds",
         quantityMin: 500,
         quantityMax: 999
     },
 
     {
-        id: 60,
-        name: "Legion",
+        id: 59,
+        name: "legion",
         quantityMin: 1000,
         quantityMax: 9999999
     },
